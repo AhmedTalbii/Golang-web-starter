681e471a02ff0ada12f07612969a3a1f6a0134de
AhmedTalbii <ahmedtalbi459@gmail.com>
2025-07-11 21:52:41 +0100

    handlError

--- controllers/homeController.go
+++ controllers/homeController.go
@@ -1,11 +1,16 @@
 package controllers
 
 import (
+	"net/http"
+
 	"Templates/config"
 	"Templates/controllers/render"
-	"net/http"
 )
 
 func HomeController(w http.ResponseWriter, r *http.Request) {
-	render.RenderPage(w,r,config.PathPages+"home.html")
+	if r.URL.Path != "/" {
+		render.RenderPage(w, r, config.PathPages+"error.html")
+		return
+	}
+	render.RenderPage(w, r, config.PathPages+"home.html")
 }
